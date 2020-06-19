cfc735d499328b8a4bfa62fb4f46732c5a68f228
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-06-19 17:01:09 +0200

    Ldap test

--- msprReseau/settings.py
+++ msprReseau/settings.py
@@ -40,43 +40,6 @@ AUTH_LDAP_USER_SEARCH = LDAPSearch(
     ldap.SCOPE_SUBTREE,
     '(uid=%(user)s)',
 )
-# Or:
-# AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=com'
-
-# Set up the basic group parameters.
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    'cn=medecins,ou=medecin,dc=lechatelet,dc=com',
-    ldap.SCOPE_SUBTREE,
-    '(objectClass=groupOfNames)',
-)
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr='cn')
-
-# Simple group restrictions
-AUTH_LDAP_REQUIRE_GROUP = 'cn=enabled,ou=medecins,ou=medecin,dc=lechatelet,dc=com'
-AUTH_LDAP_DENY_GROUP = 'cn=disabled,ou=medecins,ou=medecin,dc=lechatelet,dc=com'
-
-# Populate the Django user from the LDAP directory.
-AUTH_LDAP_USER_ATTR_MAP = {
-    'first_name': 'givenName',
-    'last_name': 'sn',
-    'email': 'mail',
-}
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    'is_active': 'cn=active,ou=medecins,ou=medecin,dc=lechatelet,dc=com',
-    'is_staff': 'cn=staff,ou=medecins,ou=medecin,dc=lechatelet,dc=com',
-    'is_superuser': 'cn=superuser,ou=medecins,ou=medecin,dc=lechatelet,dc=com',
-}
-
-# This is the default, but I like to be explicit.
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-
-# Use LDAP group membership to calculate group permissions.
-AUTH_LDAP_FIND_GROUP_PERMS = True
-
-# Cache distinguished names and group memberships for an hour to minimize
-# LDAP traffic.
-AUTH_LDAP_CACHE_TIMEOUT = 3600
 
 AUTHENTICATION_BACKENDS = [
     'django_auth_ldap.backend.LDAPBackend',
