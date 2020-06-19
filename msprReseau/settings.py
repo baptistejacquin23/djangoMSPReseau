f0dab14fdf1424216e8dea29d905d29b93fa6a73
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-06-19 11:44:31 +0200

    change host

--- msprReseau/settings.py
+++ msprReseau/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1t&kfklk=s^e-i#!+c414l-63tjsh0+9w0_4w^($1zq=50acv('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".lechatelet.com"]
+ALLOWED_HOSTS = ['.lechatelet.com']
 
 # Baseline configuration.
 AUTH_LDAP_SERVER_URI = 'ldap://172.31.3.1'
